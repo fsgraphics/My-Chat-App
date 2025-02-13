82472c98dfb262939c7fb31de14e19ae27477f67
fazlehasan <fsdigitalgraphic@gmail.com>
2025-02-13 17:36:03 +0600

    commit for Chat-Application Message 1

---
+++ app.js
@@ -0,0 +1,20 @@
+// External Imports
+const express = require("express");
+const dotenv = require("dotenv")
+// const http = require("http")
+
+
+// Internal Imports
+
+
+// configuration
+dotenv.config()
+app = express()
+
+
+
+
+//Create Server 
+app.listen(process.env.PORT, () => {
+    console.log(`app listening to port http://localhost:${process.env.PORT}`)
+})
